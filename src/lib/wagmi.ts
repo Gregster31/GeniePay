20111d9a22bd0edc7d2e5651e91c4e08c7d501c6
Gregster31 <julienhalde9@gmail.com>
2026-03-13 15:08:31 -0400

    Bug: merkel.io rate limiting bug

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -2,38 +2,22 @@ import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { mainnet, polygon, optimism, arbitrum, base, sepolia } from 'wagmi/chains';
 import { http } from 'viem';
 
-const sepoliaWithRPC = {
-  ...sepolia,
-  rpcUrls: {
-    default: {
-      http: [
-        'https://ethereum-sepolia-rpc.publicnode.com',
-        'https://rpc.sepolia.org',
-        'https://eth-sepolia.public.blastapi.io',
-        'https://sepolia.gateway.tenderly.co',
-      ]
-    },
-    public: {
-      http: [
-        'https://ethereum-sepolia-rpc.publicnode.com',
-        'https://rpc.sepolia.org',
-      ]
-    }
-  }
-};
-
 export const wagmiConfig = getDefaultConfig({
   appName: 'GeniePay',
   projectId: import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID,
-  chains: [mainnet, polygon, optimism, arbitrum, base, sepoliaWithRPC],
+  chains: [mainnet, polygon, optimism, arbitrum, base, sepolia],
+
   transports: {
-    [sepolia.id]: http('https://ethereum-sepolia-rpc.publicnode.com'),
-    [mainnet.id]: http(),
-    [polygon.id]: http(),
-    [optimism.id]: http(),
-    [arbitrum.id]: http(),
-    [base.id]: http(),
+    [mainnet.id]:  http('https://cloudflare-eth.com'),
+    [polygon.id]:  http('https://polygon-rpc.com'),
+    [optimism.id]: http('https://mainnet.optimism.io'),
+    [arbitrum.id]: http('https://arb1.arbitrum.io/rpc'),
+    [base.id]:     http('https://mainnet.base.org'),
+    [sepolia.id]:  http('https://ethereum-sepolia-rpc.publicnode.com'),
   },
+
+  pollingInterval: 60_000,
+
   ssr: false,
 });
 
